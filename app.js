3d35317c27d9805d49e4fd9d376a8f0e2c4f15b7
christian <christianjones94@gmail.com>
2022-12-28 22:01:52 -0500

    refactor- add push method to post request

--- app.js
+++ app.js
@@ -1,26 +1,30 @@
 const express = require("express");
 const app = express();
-const articles = [];
+const todoList = [
+  {
+    task1: "walk the dog",
+  },
+];
 
 app.use(express.json());
 
-app.get("/list/todo", (req, res) => {
-  console.log(req.body);
-  res.send("todoList");
+app.get("/list", (req, res) => {
+  console.log(todoList);
+  res.send(todoList);
 });
 
-app.post("/list/todo", (req, res) => {
-  console.log(req.body);
-  res.send("These are the tasks");
+app.post("/list", (req, res) => {
+  todoList.push(req.body);
+  res.send("task added");
 });
-app.patch("/list/todo", (req, res) => {
-  console.log(req.body);
-  res.send("Task updated");
+app.patch("/list", (req, res) => {
+  console.log("list updated");
+  res.send(todoList);
 });
 
 app.delete("/list", (req, res) => {
-  console.log("the list has been removed");
-  res.send("list deleted");
+  console.log("list has been deleted");
+  res.send(todoList);
 });
 
 app.listen(3000, () => {
